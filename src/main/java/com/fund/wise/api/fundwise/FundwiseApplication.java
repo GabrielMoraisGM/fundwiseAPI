75a25b6742d8f5f8a01e2c5faa9cfd1541c3693f
Gabriel Morais <gabriel.m.g.morais@gmail.com>
2023-06-21 11:34:46 -0300

    Inicio do projeto

---
+++ src/main/java/com/fund/wise/api/fundwise/FundwiseApplication.java
@@ -0,0 +1,13 @@
+package com.fund.wise.api.fundwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundwiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundwiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fund/wise/api/fundwise/FundwiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fund.wise.api.fundwise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundwiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
